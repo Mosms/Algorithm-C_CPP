2414ff9ca94b6c26c0799583cb67bcbe61dc7f5f
axis <feaka@foxmail.com>
2023-07-20 00:33:35 +0800

    Segment Tree Part Modified.

--- 图论算法/Graph Build【建图】/前向星建图-std.cpp
+++ 图论算法/Graph Build【建图】/前向星建图-std.cpp
@@ -1,32 +1,34 @@
 #include <cstdio>
 #include <cstring>
+#include <vector>
 
 const int MaxVertexNum = 1e5;
 const int MaxEdgeNum = 1e4;
 
-struct edge_node
-{
-    int next, to;
-    int weight;
-    // if the graph is unweighted, then just ignore it or set it to a constant
-    // 边无权可以直接删去或者，将其设定为固定常数即可
-} edges[MaxEdgeNum];
-int VectorStart[MaxVertexNum], cntEs = 0;
+std::vector<std::pair<int, int>> adjWeighted[MaxVertexNum];
+std::vector<int> adj[MaxVertexNum];
+int adjTop[MaxVertexNum];
+
 void GraphInit(int n)
 // Init the VectorStart so that we can use them to find the entrance
 // 初始化入口
 {
-    memset(VectorStart + 1, 0, n * sizeof(int));//注意这里1为起始坐标
+    memset(adjTop, 0, n * sizeof(int));
     return;
 }
 void AddEdge(int x, int y, int weight)
 // Add a directed edge which is from x and pointed to y with weight of weight
 // 图中加一条由 x 指向 y 的权为 weight 的有向边
 {
-    edges[++cntEs].next = VectorStart[x];
-    edges[cntEs].to = y;
-    edges[cntEs].weight = weight;
-    VectorStart[x] = cntEs;
+    adjWeighted[x][adjTop[x]++] = std::make_pair(y, weight);
+    return;
+}
+void AddEdge(int x, int y)
+// Add a directed edge which is from x and pointed to y, without weight
+// 图中加一条由 x 指向 y 的无权有向边
+{
+    adj[x][adjTop[x]++] = y;
+    return;
 }
 
 int main(void)
